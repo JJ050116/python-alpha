c7eed508b0dece1585e45193a9764a837ee985d7
JJ050116 <wachirawit050116@gmail.com>
2026-04-25 16:38:25 +0700

    Data type in python

---
+++ README.md
@@ -0,0 +1,30 @@
+### How to update code in GitHub
+- Add code
+```
+git add .
+```
+
+git add . it's means add all of file change
+
+```
+git add [file name]
+```
+git add [ file name ] add single file
+
+---
+
+- Commit code (comment code)
+```
+git commit -m "your comment"
+```
+git commit -m "" it's means comment code
+
+---
+
+- Push code (update code)
+```
+git push
+```
+git push it's means update code into GitHub
+
+---
--- week01/week01.py
+++ week01/week01.py
@@ -1,1 +1,42 @@
-# Set up environment in my device
+# Declare a variable
+# 1. variable name
+# 2. = (assignment)
+# 3. value default
+
+x = 10
+
+# warning!
+# can not declare numberic in front of the variable
+# 1x = 10
+# my-var = 20
+# my var = 30
+
+my_var = 40
+_my_var = 50
+mavar2 = 60
+
+name = 'EDCO'
+Name = 'School'
+
+# Data type in Python
+# str = string <" ", ' '>
+
+
+# Numberic
+# int = integer <1234567890>
+# float = decimal point <.1234567890>
+
+# bool = boolean <True, False> (1, 0)
+# list = []
+
+firstName = "Wachirawit"
+lastName = 'Mool-Ang'
+
+x = 100.12345
+
+isAdult = True
+
+print(type(isAdult))
+
+fullName = firstName + ' ' + lastName
+print(fullName)
